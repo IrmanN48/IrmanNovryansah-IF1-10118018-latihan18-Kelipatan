2ee465c8bb08d885439e93c24e4c14b0333dd5d4
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-10-09 22:02:10 +0700

    Add files via upload

---
+++ src/latihan18/kelipatan/Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan18.kelipatan;
+
+/**
+ *
+ * @author Aero
+ * NAMA  : IRMAN NOVRYANSAH
+ * KELAS : IF 1
+ * NIM   : 10118018
+ * Deskripsi Program : Program ini untuk menampilkan kelipatan angka yang 
+ *                     ditambahkan dengan 3.5
+ */
+public class Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //proses dan output
+        for (double i = 3.5; i <= 35; i=i+3.5 ) {
+            System.out.println(i);
+    }
+        
+    }
+}
